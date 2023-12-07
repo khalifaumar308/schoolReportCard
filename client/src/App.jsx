df14c1a68018489ababbb5893254170229e0d74f
Umar Muhammad Aminu <khalifaumar308@gmail.com>
2023-12-07 11:58:03 +0100

    Feat: create second template form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,36 +4,43 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import { Template1Form } from "./forms";
+import { Template1Form, Template2Form } from "./forms";
 import { Template1, Template2 } from "./formTemplates";
 
 
 
 function App() {
   return (
-      <Routes>
-        <Route
-          path="/download"
-          element={
-            <PDFDownloadLink document={<Template1 />} fileName="somename.pdf">
-              {({ blob, url, loading, error }) =>
-                loading ? "Loading document..." : "Download now!"
-              }
-            </PDFDownloadLink>
-          }
-        />
-        <Route path='/view' element={
+    <Routes>
+      <Route
+        path="/download"
+        element={
+          <PDFDownloadLink document={<Template1 />} fileName="somename.pdf">
+            {({ blob, url, loading, error }) =>
+              loading ? "Loading document..." : "Download now!"
+            }
+          </PDFDownloadLink>
+        }
+      />
+      <Route
+        path="/view"
+        element={
           <PDFViewer>
             <Template1 />
           </PDFViewer>
-        } />
-      <Route path='/' index element={<Template1Form />} />
-      <Route path="/t2" element={
-        <PDFViewer>
-          <Template2 />
-        </PDFViewer>
-      } />
-      </Routes>
+        }
+      />
+      <Route path="/" index element={<Template1Form />} />
+      <Route
+        path="/t2"
+        element={
+          <PDFViewer>
+            <Template2 />
+          </PDFViewer>
+        }
+      />
+      <Route path="/t2f" index element={<Template2Form />} />
+    </Routes>
   );
 }
 
--- client/src/forms/index.js
+++ client/src/forms/index.js
@@ -1,1 +1,2 @@
 export { default as Template1Form } from './Template1Form'
+export { default as Template2Form } from './Template2Form'
